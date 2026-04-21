43561ab6fc8683277073eb5f7e963791ef9b3315
Anton <ittvanposta@gmail.com>
2026-04-21 23:35:49 +0200

    123456

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200);
-  res.end('Hello from myapp1111111111111111111111111111111111 v1\n');
+  res.end('Hello from myapp oooh this runs!!! v1\n');
 });
 
 server.listen(80, () => {
